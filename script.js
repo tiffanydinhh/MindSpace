a8e31488debf0a2b9030c8f441ffe6222e31fe26
tiffanydinhh <tiffanydinhh@gmail.com>
2026-06-14 22:04:52 -0700

    tasks page

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+// ---------------- Journal Page ----------------
+
 function displayText() {
     // gets value/data from journal input and stores in variable
     const input = document.getElementById("journalInput").value;
 
     // set display text to value/data stored in variable
     document.getElementById("displayArea").textContent = input;
+}
+
+// ---------------- Tasks Page ----------------
+
+const inputTask = document.getElementById("inputTask");
+const tasksContainer = document.getElementById("tasksContainer");
+
+// user clicks add button
+function addTask() {
+    if (inputTask.value == '') {
+        alert("Please enter text");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputTask.value; // Replace HTML for list item (task) to user input task
+        tasksContainer.appendChild(li); // Append list item to tasksContainer
+
+        let x = document.createElement("span");
+        x.innerHTML = "\u00d7";
+        li.appendChild(x);
+    }
 }
